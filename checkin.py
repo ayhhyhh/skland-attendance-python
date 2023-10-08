570ed3239e0aab3e81225e27e68fa52223343646
ayhh <ayhhyhhyhh@gmail.com>
2023-10-08 15:50:49 +0800

    allow no dingtalk notification

--- checkin.py
+++ checkin.py
@@ -2,93 +2,89 @@ import json
 import sys
 import time
 import requests
-
+import random
+import os
 # 声明常量
 # 签到url post请求
 SIGN_URL = "https://zonai.skland.com/api/v1/game/attendance"
 SUCCESS_CODE = 0
-# 休眠三秒继续其他账号签到
 SLEEP_TIME = 3
 FAIL_SIGN = False
 
-# 读取cookie
-cookie_lines = sys.argv[1].split(";;")
-print("已读取" + str(len(cookie_lines)) + "个目标")
-print(str(SLEEP_TIME) + "秒后进行签到...")
-time.sleep(SLEEP_TIME)
-
-# 遍历cookie
-for cookie_line in cookie_lines:
-
-    # 准备签到信息
-    uid = cookie_line
-    signing_cookie = sys.argv[2].strip()
-    headers = {
-        "cred": signing_cookie,
-        "user-agent": "Skland/1.0.1 (com.hypergryph.skland; build:100001014; Android 25; ) Okhttp/4.11.0",
-        "Content-Type": "application/json; charset=utf-8",
-        "Accept-Encoding": "gzip",
-        "Connection": "close",
-        'platform': '1',
-    }
-
-    data = {
-        "uid": uid,
-        "gameId": 1,
-    }
-
-    # 签到请求
-    sign_response = requests.post(headers=headers, url=SIGN_URL, json=data)
-
-    # 检验返回是否为json格式
-    try:
-        sign_response_json = json.loads(sign_response.text)
-    except:
-        print(sign_response.text)
-        print("返回结果非json格式，请检查...")
-        time.sleep(SLEEP_TIME)
-        sys.exit()
-
-    # 如果为json则解析
-    code = sign_response_json.get("code")
-    message = sign_response_json.get("message")
-    data = sign_response_json.get("data")
-
-    # 返回成功的话，打印详细信息
-    if code == SUCCESS_CODE:
-        print("签到成功")
-        awards = sign_response_json.get("data").get("awards")
-        for award in awards:
-            print("签到获得的奖励ID为: " + award.get("resource").get("id"))
-            print("此次签到获得了" + str(award.get("count")) + "单位的" + award.get("resource").get("name") + "(" + award.get(
-                "resource").get("type") + ")")
-            print("奖励类型为：" + award.get("type"))
-    else:
-        if sign_response_json["message"] != "请勿重复签到！":
-            FAIL_SIGN = True
-        print(sign_response_json)
-        print("签到失败，请检查以上信息...")
-
-    # 休眠指定时间后，继续下个账户
-    time.sleep(SLEEP_TIME)
+# 读取 CRED UID
+CRED = os.environ.get("CRED")
+UID = os.environ.get("UID")
+DINGTALKTOKEN = os.environ.get("DINGTALKTOKEN")
 
 
-DingTalkToken = sys.argv[3]
+# 准备签到信息
 
-DingTalkJSON = {
-    "msgtype": 'text',
-    "text": {
-        "content": f"森空岛\n{sign_response_json}",
-    },
+headers = {
+    "cred": CRED,
+    "user-agent": "Skland/1.0.1 (com.hypergryph.skland; build:100001014; Android 25; ) Okhttp/4.11.0",
+    "Content-Type": "application/json; charset=utf-8",
+    "Accept-Encoding": "gzip",
+    "Connection": "close",
+    'platform': '1',
 }
 
-url = f"https://oapi.dingtalk.com/robot/send?access_token={DingTalkToken}"
-DingTalkResponse = requests.post(url=url, json=DingTalkJSON)
+data = {
+    "uid": UID,
+    "gameId": 1,
+}
+
+# 签到请求
+sign_response = requests.post(headers=headers, url=SIGN_URL, json=data)
+
+# 检验返回是否为json格式
+try:
+    sign_response_json = json.loads(sign_response.text)
+except:
+    print(sign_response.text)
+    print("返回结果非json格式, 请检查...")
+    time.sleep(SLEEP_TIME)
+    sys.exit()
+
+# 如果为json则解析
+code = sign_response_json.get("code")
+message = sign_response_json.get("message")
+data = sign_response_json.get("data")
+
+
+# 返回成功的话，打印详细信息
+if code == SUCCESS_CODE:
+    print("签到成功")
+    awards = sign_response_json.get("data").get("awards")
+    s = ""
+    for award in awards:
+        s += (f"签到获得的奖励ID为: {award.get('resource').get('id')} \n")
+        s += (f"此次签到获得了{award.get('count')}单位的{award.get('resource').get('name')} ({award.get('resource').get('type')})\n")
+        s += (f"奖励类型为: {award.get('type')}")
+    print(s)
+else:
+    if sign_response_json["message"] != "请勿重复签到！":
+        FAIL_SIGN = True
+    s = str(sign_response_json)
+    print(s)
+    print("签到失败，请检查以上信息...")
+
+
+
+if DINGTALKTOKEN:
+
+    DingTalkJSON = {
+        "msgtype": 'text',
+        "text": {
+            "content": f"森空岛\n{s}",
+        },
+    }
+
+    url = f"https://oapi.dingtalk.com/robot/send?access_token={DINGTALKTOKEN}"
+    DingTalkResponse = requests.post(url=url, json=DingTalkJSON)
 
 
 class AbnormalChekinException(Exception):
     pass
-
 
 if FAIL_SIGN:
     raise AbnormalChekinException("存在签到失败的账号，请检查信息")
